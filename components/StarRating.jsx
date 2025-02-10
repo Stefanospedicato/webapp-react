adf0965274b1620609126761ab45da87513fd080
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-02-10 09:35:23 +0100

    feat: fix starrating component

--- components/StarRating.jsx
+++ components/StarRating.jsx
@@ -1,6 +1,6 @@
 const StarRating = ({ vote }) => {
   const renderStars = () => {
-    return [1, 2, 3, 4, 5].map((star, index) => (
+    return [1, 2, 3, 4, 5].map((index) => (
       <i
         key={index}
         className={`bi bi-star${index < vote ? "-fill text-warning" : ""}`}
