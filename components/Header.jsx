dbb914628b4d953afb1f8950d6eb39a9d93dc057
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-02-12 12:19:42 +0100

    feat: add newstyles to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
       <header>
         <Link to="/" className="m-3 header-title">
-          FILM AREA{" "}
+          FILM AREA <i class="bi bi-camera-reels-fill"></i>
         </Link>
         <Link className="btn header-title" to="/movies/create">
           Aggiungi un film <i className="bi bi-plus"></i>
